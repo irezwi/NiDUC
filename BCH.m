7b6c97c8bd1e8814016ef78690f166ca4a1eda3d
irezwi <zwirek.ireneusz@gmail.com>
2018-05-08 10:28:59 +0200

    Computing E modified

--- BCH.m
+++ BCH.m
@@ -1,6 +1,6 @@
 m = 6; % ustawiamy parametr m zgodnie z tabel¹ kodów BCH
 n = 2^m - 1; % d³ugoœæ s³owa koduj¹cego
-k = 57; % d³ugoœæ s³owa, które kodujemy
+k = 45; % d³ugoœæ s³owa, które kodujemy
 
 msg = gf(GEN(k)); % wygenerowanie wiadomoœci
 % [genpoly, t] = bchgenpoly(n, k);
@@ -20,8 +20,10 @@ disp(decoded_message.x);
 
 wrong_bits = numel(find(msg.x ~= decoded_message.x)); % liczba bitów danych odebranych b³êdnie
 correct_bits = numel(find(msg.x == decoded_message.x)); % liczba bitów danych odebranych prawid³owo
-received_bits = length(decoded_message.x); % liczba odebranych bitów danych
-BER = wrong_bits / received_bits; % wyliczenie BER: (liczba bitów danych odebranych b³êdnie) / (liczba odebranych bitów danych)
-E = correct_bits / length(ccode); % wyliczenie E: (liczba prawid³owo odebranych bitów danych) / (liczba przes³anych bitów)
+received_data_bits = length(decoded_message.x); % liczba odebranych bitów danych
+all_received_bits = length(code); % liczba wszystkich odebranych bitów 
+
+BER = wrong_bits / received_data_bits; % wyliczenie BER: (liczba bitów danych odebranych b³êdnie) / (liczba odebranych bitów danych)
+E = correct_bits / all_received_bits; % wyliczenie E: (liczba prawid³owo odebranych bitów danych) / (liczba przes³anych bitów)
 disp(BER);
 disp(E);
