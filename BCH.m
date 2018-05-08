7ea35ee1ac2865aa8ace19300c66351f8bdf1ab4
irezwi <zwirek.ireneusz@gmail.com>
2018-05-08 10:22:45 +0200

    computing E added

--- BCH.m
+++ BCH.m
@@ -4,7 +4,7 @@ k = 57; % d³ugoœæ s³owa, które kodujemy
 
 msg = gf(GEN(k)); % wygenerowanie wiadomoœci
 % [genpoly, t] = bchgenpoly(n, k);
-t = 15; 
+% t = 15; 
 
 % zakodowanie BCH
 code = bchenc(msg, n, k);
@@ -19,6 +19,9 @@ disp(msg.x);
 disp(decoded_message.x);
 
 wrong_bits = numel(find(msg.x ~= decoded_message.x)); % liczba bitów danych odebranych b³êdnie
+correct_bits = numel(find(msg.x == decoded_message.x)); % liczba bitów danych odebranych prawid³owo
 received_bits = length(decoded_message.x); % liczba odebranych bitów danych
-BER = wrong_bits / received_bits; % wyliczenie BER
-
+BER = wrong_bits / received_bits; % wyliczenie BER: (liczba bitów danych odebranych b³êdnie) / (liczba odebranych bitów danych)
+E = correct_bits / length(ccode); % wyliczenie E: (liczba prawid³owo odebranych bitów danych) / (liczba przes³anych bitów)
+disp(BER);
+disp(E);
